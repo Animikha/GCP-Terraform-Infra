95b4886f2de20270a148c78bbe2eb1ab82e30170
Abhinav522 <106406612+Abhinav522@users.noreply.github.com>
2025-08-12 13:14:16 +0100

    Delete modules/vm/outputs.tf

--- modules/vm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_self_link" {
-  value = google_compute_instance.vm.self_link
-}
-
-output "vm_public_ip" {
-  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
-}
